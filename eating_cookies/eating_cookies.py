7458cec588756d58ed1ef7eeb6e9f41432991efe
m-j-hacker <mjhacker@gmail.com>
2019-05-09 16:43:18 -0500

    Starting position for cookies section, took notes

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,11 +6,22 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  total_cookies = n
+  number_of_ways = 0
+  if total_cookies < 0:
+    return 'Ivalid input, number of cookies must be a positive integer!'
 
+  if total_cookies == 0:
+    return 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_cookies = int(sys.argv[1])
     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
   else:
     print('Usage: eating_cookies.py [num_cookies]')
+
+    # Notes
+    # The problem boils down to this - how many different times can
+    # the given number be reduced by 1, 2, and 3?
+    # 
+    # Recursion holds the answer 
